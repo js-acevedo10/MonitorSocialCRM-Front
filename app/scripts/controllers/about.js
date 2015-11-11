c1a18832b603d966f126ec0578c9e817311d3a66
js-acevedo10 <js.acevedo10@uniandes.edu.co>
2015-11-11 02:12:25 -0500

    Corrección de errores

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,7 +8,20 @@
  * Controller of the monitorSocialCrmFrontApp
  */
 angular.module('monitorSocialCrmFrontApp')
-  .controller('AboutCtrl', function () {
+  .controller('AboutCtrl', function ($scope, $http) {
+    $scope.errors = {};
+    $http.get('https://monitor-prueba-social-1.herokuapp.com/mensajes')
+        .then(
+        function(data) {
+            $scope.herokuMessage = data;    
+        },
+        function(err) {
+            $scope.errors.herokuError = {
+                encabezado : 'Error en el servidor:',
+                error : err
+            };
+        });
+    
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
