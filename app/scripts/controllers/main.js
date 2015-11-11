71c6efc5021220b1349d825a938b21a12eb90d3f
js-acevedo10 <js.acevedo10@uniandes.edu.co>
2015-11-11 16:52:17 -0500

    Index Reference to Main

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,13 +2,34 @@
 
 /**
  * @ngdoc function
- * @name monitorSocialCrmFrontApp.controller:MainCtrl
+ * @name monitorSocialCrmFrontApp.controller:AboutCtrl
  * @description
- * # MainCtrl
+ * # AboutCtrl
  * Controller of the monitorSocialCrmFrontApp
  */
 angular.module('monitorSocialCrmFrontApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $http, $timeout) {
+    $scope.errors = {};
+    $scope.numLlamadas = 0;
+    
+    var herokk = function() {
+            $http.get('https://monitor-prueba-social-1.herokuapp.com/mensajes')
+            .then(
+            function(data) {
+                $scope.herokuMessage = data.data;    
+            },
+            function(err) {
+                $scope.errors.herokuError = {
+                    encabezado : 'Error en el servidor:',
+                    error : err
+                };
+            });
+        $scope.numLlamadas = $scope.numLlamadas + 1;
+        $timeout(herokk, 5000);
+    };
+    
+    $timeout(herokk, 5000);
+    
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
